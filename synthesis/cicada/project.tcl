741ea7579a0bff9ccfc396f77ee195e62280c991
Adrian Alan Pol <adrianalan.pol@gmail.com>
2023-03-30 18:14:22 +0200

    Remove generated files

--- synthesis/cicada/project.tcl
+++
@@ -1,8 +0,0 @@
-variable project_name
-set project_name "myproject"
-variable backend
-set backend "vivado"
-variable part
-set part "xc7vx690tffg1927-2"
-variable clock_period
-set clock_period 6.25
